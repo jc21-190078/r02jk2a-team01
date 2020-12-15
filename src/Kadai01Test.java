8fccac9c7c5a80fe9ed73354621db03b80a5287f
190455 <190455@e0505-02.JC-21.local>
2020-12-15 14:15:46 +0900

    編集済み

--- src/Kadai01Test.java
+++ src/Kadai01Test.java
@@ -7,8 +7,11 @@ class Kadai01Test {
 	@Test
 	void testReverseString() {
 		Kadai01 k = new Kadai01();
-		assertNull(k.ReverseString(null));
-		assertEquals(4321,k.ReverseString("1234"));
+//		assertNull(k.ReverseString(null));
+		assertEquals("4321",k.ReverseString("1234"));
+		assertEquals("FEDCBA",k.ReverseString("ABCDEF"));
+		assertEquals("5 4A 6",k.ReverseString("6 A4 5"));
+
 	}
 
 }
